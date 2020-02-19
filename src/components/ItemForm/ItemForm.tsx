3c66465d77ba22e7d3c48dba25536352600d8223
Robert Osborne <robertosborne@Roberts-Mac-mini.local>
2020-02-18 21:24:23 -0800

    Update preitter

--- src/components/ItemForm/ItemForm.tsx
+++ src/components/ItemForm/ItemForm.tsx
@@ -2,36 +2,36 @@ import * as React from 'react'
 import { ShoppingItemContext } from '../../context'
 
 export const ItemForm: React.FC = React.memo(
-  (): JSX.Element => {
-    const [shoppingItem, setShoppingItem] = React.useState<string>('')
-    const { addItem } = React.useContext(ShoppingItemContext)
+    (): JSX.Element => {
+        const [shoppingItem, setShoppingItem] = React.useState<string>('')
+        const { addItem } = React.useContext(ShoppingItemContext)
 
-    const onSubmit = (e: React.FormEvent<EventTarget>): void => {
-      e.preventDefault()
-      addItem(shoppingItem)
+        const onSubmit = (e: React.FormEvent<EventTarget>): void => {
+            e.preventDefault()
+            addItem(shoppingItem)
+        }
+
+        return (
+            <form onSubmit={onSubmit}>
+                <fieldset>
+                    <label htmlFor="shoppingItem">
+                        <input
+                            id="shoppingItem"
+                            value={shoppingItem}
+                            onChange={e => setShoppingItem(e.target.value)}
+                        />
+                    </label>
+
+                    <button type="submit">Submit</button>
+                </fieldset>
+                <style jsx>
+                    {`
+                        fieldset {
+                            border: none;
+                        }
+                    `}
+                </style>
+            </form>
+        )
     }
-
-    return (
-      <form onSubmit={onSubmit}>
-        <fieldset>
-          <label htmlFor="shoppingItem">
-            <input
-              id="shoppingItem"
-              value={shoppingItem}
-              onChange={e => setShoppingItem(e.target.value)}
-            />
-          </label>
-
-          <button type="submit">Submit</button>
-        </fieldset>
-        <style>
-          {`
-                    fieldset {
-                        border: none;
-                    }
-                `}
-        </style>
-      </form>
-    )
-  }
 )
