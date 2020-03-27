56290d9fb650fda7226c58bdd20ce378ea301f98
Robert Osborne <robertosborne@Roberts-Mac-mini.local>
2020-03-26 20:30:29 -0700

    Update config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,4 @@
 const dotenv = require('dotenv')
-const withCSS = require('@zeit/next-css')
-const withPWA = require('next-pwa')
 
 dotenv.config()
 
@@ -10,27 +8,20 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
     enabled: process.env.ANALYZE === 'true'
 })
 
-module.exports = withPWA(
-    withCSS(
-        withBundleAnalyzer({
-            env: {
-                AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
-                AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
-                AUTH0_CLIENT_SECRET: process.env.AUTH0_CLIENT_SECRET,
-                AUTH0_SCOPE: 'openid profile',
-                REDIRECT_URI: isDev
-                    ? process.env.DEV_REDIRECT_URI
-                    : process.env.REDIRECT_URI,
-                POST_LOGOUT_REDIRECT_URI: isDev
-                    ? process.env.DEV_POST_LOGOUT_REDIRECT_URI
-                    : process.env.POST_LOGOUT_REDIRECT_URI,
-                SESSION_COOKIE_SECRET: process.env.SESSION_COOKIE_SECRET,
-                SESSION_COOKIE_LIFETIME: 7200 // 2 hours
-            },
-            pwa: {
-                dest: 'public'
-            },
-            target: 'serverless'
-        })
-    )
-)
+module.exports = withBundleAnalyzer({
+    env: {
+        AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
+        AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
+        AUTH0_CLIENT_SECRET: process.env.AUTH0_CLIENT_SECRET,
+        AUTH0_SCOPE: 'openid profile',
+        REDIRECT_URI: isDev
+            ? process.env.DEV_REDIRECT_URI
+            : process.env.REDIRECT_URI,
+        POST_LOGOUT_REDIRECT_URI: isDev
+            ? process.env.DEV_POST_LOGOUT_REDIRECT_URI
+            : process.env.POST_LOGOUT_REDIRECT_URI,
+        SESSION_COOKIE_SECRET: process.env.SESSION_COOKIE_SECRET,
+        SESSION_COOKIE_LIFETIME: 7200 // 2 hours
+    },
+    target: 'serverless'
+})
